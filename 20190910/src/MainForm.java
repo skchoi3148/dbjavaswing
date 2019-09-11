5ac58940b7915d3f5aa21a06ad43f671231e3c5a
305 <305@305-PC>
2019-09-11 10:25:40 +0900

    aaa

--- 20190910/src/MainForm.java
+++ 20190910/src/MainForm.java
@@ -1,18 +1,25 @@
 import java.awt.Color;
 import java.awt.Font;
-import java.awt.ScrollPane;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.Vector;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
-import javax.swing.table.TableColumn;
 
 public class MainForm extends JFrame {
 	private JTable table;
+	private DefaultTableModel dtModel;
+	private Vector<String> vec_column = new Vector<>();
 
 	public MainForm() {
 		setTitle("MainForm");
@@ -31,25 +38,74 @@ public class MainForm extends JFrame {
 		getContentPane().add(panel);
 		panel.setLayout(null);
 
-		JButton btnNewButton = new JButton("SelectMember");
-		btnNewButton.setFont(new Font("궁서체", Font.BOLD, 17));
-		btnNewButton.setBounds(300, 127, 159, 51);
-		panel.add(btnNewButton);
-		Vector<String> vec = new Vector<>();
-		vec.addElement("TEST");
-		vec.addElement("TEST");
-		vec.addElement("TEST");
+		JButton btnSelect = new JButton("SelectMember");
+		btnSelect.setFont(new Font("궁서체", Font.BOLD, 17));
+		btnSelect.setBounds(300, 127, 159, 51);
+		panel.add(btnSelect);
 
-		DefaultTableModel dtModel = new DefaultTableModel(0, 0);
-		dtModel.addRow(new Object[] { "111", "222", "333" });
+		btnSelect.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				dtModel.setDataVector(new Vector<String>(), vec_column);
+				/*
+				 * 1. ojdbc6.jar 추가 2. class.forName 오라클이면 오라클 드라이버 class 확인 3. Connection DB 연결
+				 * 4. PreapareStatement sql 구문 작성 5. ResultSet rs 에 테이블에 내용을 반환 6. connection
+				 * pstmt rs close 자원읽기 속성 해제
+				 */
+				Connection conn = null;
+				PreparedStatement pstmt = null;
+				ResultSet rs = null;
+				try {
+					Class.forName("oracle.jdbc.driver.OracleDriver");
+					conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "hr", "1234");
+					pstmt = conn.prepareStatement("select * from member");
+					rs = pstmt.executeQuery();
+					while (rs.next()) {
+						System.out.println("rs.getString(1) =" + rs.getString(1));
+						System.out.println("rs.getString(2) =" + rs.getString(2));
+						System.out.println("rs.getString(3) =" + rs.getString(3));
+						System.out.println("rs.getString(4) =" + rs.getString(4));
+						Object[] temp = new Object[] { 
+								rs.getString(1), 
+								rs.getString(2), 
+								rs.getString(3),
+								rs.getString(4) 
+								};
+						dtModel.addRow(temp);
+						dtModel.fireTableDataChanged();
+					}
+				} catch (Exception ex) {
+					ex.printStackTrace();
+				} finally {
+					try {
+						if (rs != null)
+							rs.close();
+						if (pstmt != null)
+							pstmt.close();
+						if (conn != null)
+							conn.close();
+					} catch (Exception ee) {
+					}
+				}
+			}
+		});
 
+		vec_column.addElement("ID");
+		vec_column.addElement("PW");
+		vec_column.addElement("COMMENTA");
+		vec_column.addElement("GENDER");
+
+		dtModel = new DefaultTableModel(vec_column, 0);
+		dtModel.addRow(new Object[] { "111", "222", "333", "444" });
+
+		table = new JTable();
 		table.setModel(dtModel);
 		table.setRowHeight(40);
 		table.setBounds(22, 10, 252, 149);
-		
-		ScrollPane sp = new ScrollPane();
+
+		JScrollPane sp = new JScrollPane(table);
 		sp.setBounds(22, 10, 256, 153);
-		sp.add(table);
+		// sp.add(table);
 
 		panel.add(sp);
 
